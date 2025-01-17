0bad1c9b700fd54092169f67b8c394a69d666e4d
LagAntunes <lag151617@gmail.com>
2025-01-17 16:28:20 -0300

    Primeiro commit do projeto e adicionando React e outras funcionalidades

---
+++ landing-page-garcia-viagens/app/components/Webdoor/main.tsx
@@ -0,0 +1,10 @@
+const Webdoor = () => {
+    return (
+        <div className="bg-green-500 p-4">
+            {/*classes acima vindas do css tailwind*/}
+            <p>Um webdoor aqui</p>
+        </div>
+    )
+}
+
+export default Webdoor;
---
+++ landing-page-garcia-viagens/app/page.tsx
@@ -0,0 +1,9 @@
+import Webdoor from "../app/components/Webdoor/main"
+
+export default function Home() {
+  return (
+    <div>
+      <Webdoor/>
+    </div>
+  );
+}
